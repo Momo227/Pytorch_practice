826d9b1dab97461086cb69a73684bbdbda218cff
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2021-11-18 16:41:01 +0900

    add lesson5

---
+++ lesson5/DistilBERT/mkdata21.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: sjis -*-
+
+from transformers import AutoTokenizer
+import pickle
+import re
+
+# Laboro”Å‚ğ—˜—p‚·‚é
+tknz = AutoTokenizer.from_pretrained("cl-tohoku/bert-base-japanese-whole-word-masking")
+
+xdata, ydata = [],[]
+with open('train.tsv','r',encoding='utf-8') as f:
+    for line in f:
+        line = line.rstrip()
+        # re.matchF•¶š—ñ‚Ìæ“ª‚ªƒ}ƒbƒ`‚·‚é‚©ƒ`ƒFƒbƒNA’Šo
+        # ^:•¶š—ñ‚Ìæ“ª, \d:”CˆÓ‚Ì”š, +:‚P‰ñˆÈã‚ÌŒJ‚è•Ô‚µ, \t:‹ó”’•¶š‚Å‚Í‚È‚¢”CˆÓ‚Ì•¶š, .:”CˆÓ‚Ìˆê•¶š, ?:‚O‰ñ‚Ü‚½‚Í‚P‰ñ, $:•¶š—ñ‚Ì––”ö
+        result = re.match('^(\d+)\t(.+?)$', line)
+        ydata.append(int(result.group(1)))
+        sen = result.group(2)
+        tid = tknz.encode(sen)
+        if (len(tid) > 512):  # Å‘å’·‚Í 512
+            tid = tid[:512]
+        xdata.append(tid)
+
+with open('xtrain.pkl','bw') as fw:
+    pickle.dump(xdata,fw)
+
+with open('ytrain.pkl','bw') as fw:
+    pickle.dump(ydata,fw)
